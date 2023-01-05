dcb94e23d5b04546adb7e4e4f2abb3105686ebe3
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-01-05 18:13:17 +0900

    clean up

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,10 +1,4 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
-console.log("hi")
-
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke('ping'),
-})
+console.log("[preload]: start")
+console.log("[preload]: end")
